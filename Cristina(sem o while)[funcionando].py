680c61a70935d86b7f0bb3c5e9fa59811ac80044
vi-mat <72581547+vi-mat@users.noreply.github.com>
2020-10-08 20:42:21 -0300

    Update Cristina(sem o while)[funcionando].py

--- Cristina(sem o while)[funcionando].py
+++ Cristina(sem o while)[funcionando].py
@@ -4,10 +4,10 @@ r = sr.Recognizer()
 
 with sr.Microphone() as s:
     r.adjust_for_ambient_noise(s)
-    chat = open('chat.txt', 'w')
-    chat.write(f"Orador:")
-    audio = r.listen(s)
-    speech =r.recognize_google(audio, language='pt')
-    print(speech)
-    chat = open('chat.txt', 'w')
-    chat.write(f"Orador: {speech}")
+    while True:
+        audio = r.listen(s)
+        speech =r.recognize_google(audio, language='pt')
+        print(speech)
+        chat = open('chat.txt', 'a')
+        chat.write(f"Orador: {speech}\n")
+        chat.close()
