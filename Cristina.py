0819993797c48f32a412f7b0a7a11d5ac7f660c2
João Pedro Olimpio <olimpiojp@hotmail.com>
2020-10-16 11:34:06 -0300

    Update Cristina.py

--- Cristina.py
+++ Cristina.py
@@ -1,10 +1,19 @@
 import speech_recognition as sr
 r = sr.Recognizer()
-with sr.Microphone() as s:
-	r.adjust_for_ambient_noise(s)
+with sr.Microphone() as source:
+	r.adjust_for_ambient_noise(source)
 	while True:
- 		audio = r.listen(s)
- 		speech = r.recognize_google(audio, language='pt')
- 		print(speech) 		
- open('chat.txt', "w"):
- chat.write("Orador: ", speech)
+		print('Diga algo!')
+		audio = r.listen(source)
+		try: 
+			speech = r.recognize_google(audio, language='pt')
+			print(speech)
+			arquivo = open('chat.txt', 'a')
+			s = 'Orador: {fala}\n'.format(fala = speech)
+			arquivo.write(s)
+			arquivo.close()
+		except sr.UnknownValueError:
+			print('Google nao pode reconhecer o que foi dito')
+		except sr.RequestError as e:
+			print('Os servicos Google requisitados estao indisponiveis; {0}'.format(e))		
+
