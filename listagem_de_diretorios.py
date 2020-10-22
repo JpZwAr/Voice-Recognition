fed84637c3b90554f482776cdc70dcee1e2da72d
vi-mat <72581547+vi-mat@users.noreply.github.com>
2020-10-22 15:57:30 -0300

    Update listagem_de_diretorios.py
    Resolvido problema de acesso negado

--- listagem_de_diretorios.py
+++ listagem_de_diretorios.py
@@ -1,4 +1,7 @@
-from os import chdir, listdir, path
+from os import listdir, path
+
+raiz = 'C:/ProgramData/Microsoft/Windows/Start Menu/Programs'
+saida = 'C:/Users/Victor Mateus/Desktop'
 
 
 def fileext(nome):
@@ -12,31 +15,42 @@ def fileext(nome):
 
 
 def scandir(root, output):
+    pastas = 0
+    pastas_negadas = []
+    arquivos = []
     dires = []
     diret = root
-    chdir(output)
-    file = open('Output.txt', 'w')
-    chdir(diret)
+    file = open(f'{output}/Output.txt', 'w')
     while True:
+        print(f'{pastas} pastas analisadas')
+        while True:
+            try:
+                indice = listdir(diret)
+                break
+            except PermissionError:
+                pastas_negadas.append(diret)
+                diret = dires.pop()
         diret_qnt = 0
-        arquivos = []
-        for item in listdir():
+        arquivos.clear()
+        for item in indice:
             if path.isdir(f'{diret}/{item}'):
                 dires.insert(len(dires) - diret_qnt, f'{diret}/{item}')
                 diret_qnt += 1
-                pastas += 1
             else:
-                arquivos.insert(0, f'    Arquivo {fileext(item)}: {item}\n')
+                arquivos.append(f'    Arquivo {fileext(item)}: {item}\n')
         if len(arquivos) > 0:
-            file.write(f'{diret}:')
-            file.write('\n')
             arquivos.sort()
-            arquivos.reverse()
-            for _ in arquivos:
-                file.write(f'{arquivos.pop()}')
+            file.write(f'{diret}:\n')
+            for item in arquivos:
+                file.write(item)
             file.write('\n\n')
-        if len(dires) > 0:
-            diret = dires.pop()
-            chdir(diret)
-        else:
+        pastas += 1
+        if not dires:
+            if pastas_negadas:
+                file.write(f'Não foi possível obter acesso a:\n    {pastas_negadas}')
+            file.close()
             break
+        diret = dires.pop()
+
+
+scandir(raiz, saida)
