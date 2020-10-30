6f9da741dce15e33fb240c177648b699f762e659
João Pedro Olimpio <olimpiojp@hotmail.com>
2020-10-30 16:06:58 -0300

    Update Cristina.py

--- Cristina.py
+++ Cristina.py
@@ -6,69 +6,57 @@ import speech_recognition as sr
 
 ####################################################################################################################################
 
-raiz = ['C:/Users/%%%%Seu Usuário%%%%/AppData/Roaming/Microsoft/Windows/Start Menu',
-        'C:/ProgramData/Microsoft/Windows/Start Menu/Programs']
-saida = 'C:/Users/%%%%Seu Usuário%%%%/Desktop'
+class Abrir:
 
-def fileext(nome):  # --------------------------------------> função que retorna uma string com a etensão do arquivo
-    ext = ''
-    try:
-        for i in range(len(nome) - list(nome)[::-1].index('.') - 1, len(nome)):  # lê o nome do arquivo de trás
-                                                                                # pra frente até primeiro ponto
-            ext = ext + nome[i]
-        return ext
-    except ValueError:  # ----------------------------------> algumas raras exceções não possuem extensão
-        return ''
+	def __init__(self):
+		self.raiz = ['C:/Users/Joãozinho/AppData/Roaming/Microsoft/Windows/Start Menu',
+						'C:/ProgramData/Microsoft/Windows/Start Menu/Programs']
+		self.dicio_programas = self.scandir(self.raiz)
+
+	def fileext(self, nome):  # --------------------------------------> função que retorna uma string com a extensão do arquivo
+		ext = ''
+		try:
+			for i in range(len(nome) - list(nome)[::-1].index('.') - 1, len(nome)):  # lê o nome do arquivo de trás
+																					# pra frente até o primeiro ponto
+				ext = ext + nome[i]
+			return ext
+		except ValueError:  # ----------------------------------> algumas raras exceções não possuem extensão
+			return ''
 
 
-def scandir(root, output):
-    pastas = 0  # --------------------------------------> contador de pastas analisadas
-    pastas_negadas = []  # -----------------------------> lista de pastas sem permissão de acesso
-    arquivos = []  # -----------------------------------> lista de arquivos no diretório analisado
-    dires = []  # --------------------------------------> lista de diretórios a serem analisados
-    dicio = {}  # --------------------------------------> dicionário de indexação
-    diret = root.pop()  # ------------------------------> obtém o diretório incial
-    file = open(f'{output}/Output.txt', 'w')  # --------> abre o arquivo de log (sem muita utilidade, nesse código)
-    while True:
-        while True:  # ---------------------------------> loop para verificar erros de permissão de acesso
-            try:
-                indice = listdir(diret)  # -------------> lista o diretorio e o coloca na variavel indice
-                break
-            except PermissionError:
-                pastas_negadas.append(diret)
-                diret = dires.pop()
-        diret_qnt = 0
-        arquivos.clear()
-        for item in indice:
-            if path.isdir(f'{diret}/{item}'):  # ---------------------------> verifica se o item listado é um diretório
-                dires.insert(len(dires) - diret_qnt, f'{diret}/{item}')  # -> insere os diretórios
-                                                                                # (sempre no final da lista, ordenados)
-                diret_qnt += 1
-            else:
-                if fileext(item) == '.lnk':  # -----------------------------> verifica se o arquivo é um atalho
-                    arquivos.append(f'{item[:len(item)-4]}'.lower()) # -----> adiciona o arquivo na lista de arquivos,
-                                                                                # em letras minusculas e sem a extensão
-        if len(arquivos) > 0:  # -------------------------------------------> verifica se há arquivos no diretório
-            arquivos.sort()  # ---------------------------------------------> organiza os arquivos em ordem alfabética
-            file.write(f'{diret}:\n')  # -----------------------------------> escreve o nome do diretório no .txt
-            for item in arquivos:
-                file.write(item)  # ----------------------------------------> escreve os arquivos no .txt
-            file.write('\n\n')
-            dicio.update({}.fromkeys(arquivos, diret))  # ------------------> adiciona os arquivos ao
-                                                                                # dicionario de indexação
-        pastas += 1
-        if not dires:
-            if pastas_negadas:
-                file.write(f'Não foi possível obter acesso a:\n    {pastas_negadas}')  # ---> printa as pastas negadas
-                                                                                                # no .txt
-            # print(dicio)
-            if not len(root):  # ------------------------------------------->  # verifica se ainda há raizes a consultar
-                file.close()
-                return dicio
-            else:
-                dires.append(root.pop())
-        diret = dires.pop()
+	def scandir(self, root):
+		pastas_negadas = []
+		programas = {}
+		dires = root.copy()
+		diret = dires.pop()
+		while True:
+			while True:
+				try:
+					indice = listdir(diret)
+					break
+				except PermissionError:
+					pastas_negadas.append(diret)
+					diret = dires.pop()
+			diret_qnt = 0
+			for item in indice:
+				if path.isdir(f'{diret}/{item}'):
+					dires.insert(len(dires) - diret_qnt, f'{diret}/{item}')
+					diret_qnt += 1
+				else:
+					if self.fileext(item) == '.lnk':
+						programas.update({f'{item[:len(item) - 4]}'.lower(): diret})
+			if not dires:
+				return programas
+			diret = dires.pop()
 
+###################################################################################################################################
+
+class Pesquisar:
+
+	def __init__(self):
+		self.site = 'www.google.com/search?q='
+		self.item_pesquisa = ''
+		
 ###################################################################################################################################
 
 r = sr.Recognizer()
@@ -94,31 +82,46 @@ with sr.Microphone() as source:
 			comando = speech.split()
 			if comando[0] in chaves:
 				if comando[0] == chaves[0]:
-					
-					indice = scandir(raiz, saida)  # -------------------------------------> obtém o dicionario de indexação
+
+					indice = Abrir()
 
 					fala = ' '.join(comando[1:len(comando)])
-					if indice.get(fala):  # ----------------------------------------------> verifica se o arquivo especificado tem
-																								# correspondencia no dicionario
-						startfile(f'{indice.get(fala)}/{fala}.lnk')
+                    
+					if indice.dicio_programas.get(fala):  # ----------------------------------------> verifica se o arquivo
+																									# especificado tem
+																									# correspondencia no dicionario
+						startfile(f'{indice.dicio_programas.get(fala)}/{fala}.lnk')
+                        
 					else:
 						parecidos = []
-						for item in indice.keys():   # -----------------------------------> pesquisa cada palavra da fala do usuário
-							for frag_fala in fala.split():                                      # nas chaves do dicionário
+						for item in indice.dicio_programas.keys():   # -----------------------------> pesquisa cada palavra da
+							                                                                        # fala do usuário
+							for frag_fala in fala.split():                                          # nas chaves do dicionário
 								if frag_fala in item:
 									parecidos.append(item)  # ----------------------------> se houver potenciais correspondencias,
-																								# adiciona na lista 'parecidos'
+																						  # adiciona na lista 'parecidos'
 
 						if len(parecidos) == 1:
-							startfile(f'{indice.get(parecidos[0])}/{parecidos[0]}.lnk')  # - se houver apenas uma correspondência, a inicia
+							startfile(f'{indice.dicio_programas.get(parecidos[0])}/{parecidos[0]}.lnk')  # - se houver apenas uma
+																										 # correspondência, a inicia
 
-						elif len(parecidos) > 1:  # ------------------------------------->  se houver mais de uma correspondência, pede
-																								# ajuda ao usuário
+						elif len(parecidos) > 1:  # -------------------------------------> se houver mais de uma correspondência,
+																						 # pede ajuda ao usuário
 							print(f'{parecidos}\n')
 							print('Não encontrei o item solicitado. Alguma das opções acima é o que você deseja? ')
 							if input().lower() == 'sim':
 								resp = int(input('Qual indice? '))
-								startfile(f'{indice.get(parecidos[resp])}/{parecidos[resp]}.lnk')
+								startfile(f'{indice.dicio_programas.get(parecidos[resp])}/{parecidos[resp]}.lnk')
 						else:
-							print('Não encontrado.')	
-
+							print('Não encontrado.')
+				if comando[0] == chaves[4]:
+					
+					pesquisa = Pesquisar()
+					
+					fala = ' '.join(comando[1:len(comando)])
+					
+					pesquisa.item_pesquisa = fala
+					
+					pesquisa.item_pesquisa.replace(' ','+')
+					
+					startfile(pesquisa.site + pesquisa.item_pesquisa)
